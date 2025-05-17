c9932e3a332ca589bcb7c67fca2c29ec9a9988e6
tk <t.khamiam@gmail.com>
2025-05-18 00:16:13 +0700

    feat(hero): updated hero text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,14 +15,28 @@ function Home() {
         style={{ minHeight: `calc(100vh - 64px - 64px)` }}
         className="flex flex-col justify-center"
       >
-        <h1 className="text-6xl font-bold font-sans">Hi, this is Tammy.</h1>
+        <h1 className="text-6xl font-bold font-sans">Hello, this is Tammy.</h1>
         <div className="pt-18 space-x-2">
           <span className="text-3xl pt-8">Currently</span>
           <MoveRight className="inline" />
           <span className="text-2xl">Junior Software Developer Bootcamp</span>
-          <p className="pt-4">Promising Junior Software Developer, cultivated practical experience in game development over years.</p>
-          <p>My design perspective allows for imaginative <b>problem-solving</b> and a considered approach to user-centric design.</p>
-          <p>I am keen to contribute meaningfully to product development while continuing my learning journey alongside experienced team members.</p>
+          <p className="pt-4">
+            I’ve spent the past few years leveling up through hands-on game dev
+            and passion projects
+          </p>
+          <p>
+            — learning by building and breaking{" "}
+            <i>(and occasionally debugging after midnight)</i>.
+          </p>
+          <p>My background in design means I think in both pixels and logic.</p>
+          <p>
+            I love solving problems with a user-first mindset and a drip of
+            imagination.
+          </p>
+          <p>
+            I’m excited to keep growing, deliver cool things, and learn from folks
+            who are just as nerdy about tech as I am.
+          </p>
         </div>
       </div>
       <div id="work-section" className="mt-8">
