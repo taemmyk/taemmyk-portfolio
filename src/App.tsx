f41654227abb2d84e820e1ac3ab23b342dee0b7a
tk <t.khamiam@gmail.com>
2025-07-30 10:10:32 +0700

    fix(router): app router discarded

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-import StopMotion from "./pages/stopmotion";
 import Content from "./pages/content";
 
 function App() {
   return (
     <>
-      <Routes>
-      <Route path="/" element={<StopMotion />} />
-      <Route path="/me" element={<Content />} />
-    </Routes>
+      <Content />
     </>
   );
 }
